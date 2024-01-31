18da9c0044c0ea57ea32d179f8334e7000b09c70
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-01 00:55:32 +0600

    added controller and routes

---
+++ routes/api.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import AuthController from "../controllers/authController.js";
+
+const router = Router();
+
+router.post("/auth/register", AuthController.register);
+
+export default router;
