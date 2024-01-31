8be7b4ccd089a5969474954040a7b85d81713d6e
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-01 01:41:28 +0600

    Signup complete

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 import prisma from "../DB/db.config.js";
 import vine, { errors } from "@vinejs/vine";
 import { registerSchema } from "../validations/authValidaiton.js";
+import bcrypt from "bcrypt";
 
 class AuthController {
   static async register(req, res) {
@@ -8,11 +9,45 @@ class AuthController {
       const body = req.body;
       const validator = vine.compile(registerSchema);
       const payload = await validator.validate(body);
-      return res.json({ payload });
+
+      // check if email exist
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+
+      if (findUser) {
+        return res.status(400).json({
+          error: {
+            email: "Email already taken, please try with another email",
+          },
+        });
+      }
+
+      //ecrypt the password
+      const salt = bcrypt.genSaltSync(10);
+      payload.password = bcrypt.hashSync(payload.password, salt);
+
+      const user = await prisma.users.create({
+        data: payload,
+      });
+
+      return res.json({
+        status: 200,
+        messages: "User Created Successfully",
+        user,
+      });
     } catch (error) {
+      console.log("error is", error);
       if (error instanceof errors.E_VALIDATION_ERROR) {
         // console.log(error.messages);
         return res.status(400).json({ errors: error.messages });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "something went wrong,, please try again later",
+        });
       }
     }
   }
