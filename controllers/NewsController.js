8be0517b939b3c08c34c3f5bd90c1ee13640fa52
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-01 21:00:57 +0600

    news validations

---
+++ controllers/NewsController.js
@@ -0,0 +1,7 @@
+class NewsController {
+  static async index(req, res) {}
+  static async store(req, res) {}
+  static async show(req, res) {}
+  static async update(req, res) {}
+  static async destroy(req, res) {}
+}
