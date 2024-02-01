88d809c29918325f7b315538c08960af13b9f5bc
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-01 10:43:27 +0600

    added login function with bcrypt

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,6 @@
 import prisma from "../DB/db.config.js";
 import vine, { errors } from "@vinejs/vine";
-import { registerSchema } from "../validations/authValidaiton.js";
+import { loginSchema, registerSchema } from "../validations/authValidaiton.js";
 import bcrypt from "bcrypt";
 
 class AuthController {
@@ -38,6 +38,52 @@ class AuthController {
         messages: "User Created Successfully",
         user,
       });
+    } catch (error) {
+      console.log("error is", error);
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        // console.log(error.messages);
+        return res.status(400).json({ errors: error.messages });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "something went wrong,, please try again later",
+        });
+      }
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const body = req.body;
+      const validator = vine.compile(loginSchema);
+      const payload = await validator.validate(body);
+
+      //find user
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+
+      if (findUser) {
+        if (!bcrypt.compareSync(payload.password, findUser.password)) {
+          return res.status(400).json({
+            error: {
+              email: "Invalid Credentials",
+            },
+          });
+        }
+
+        return res.json({ message: "Logged In" });
+      }
+
+      return res.status(400).json({
+        error: {
+          email: "no user found with this email",
+        },
+      });
+
+      return res.json({ payload });
     } catch (error) {
       console.log("error is", error);
       if (error instanceof errors.E_VALIDATION_ERROR) {
