372b8c8d0b2b5edbfeffff35437d3cae4cab2cae
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-01 11:34:39 +0600

    Profile Controller Route

---
+++ controllers/ProfileController.js
@@ -0,0 +1,16 @@
+class ProfileController {
+  static async index(req, res) {
+    try {
+      const user = req.user;
+      return res.json({ status: 200, user });
+    } catch (error) {
+      return res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+  static async store() {}
+  static async show() {}
+  static async update() {}
+  static async destroy() {}
+}
+
+export default ProfileController;
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import AuthController from "../controllers/authController.js";
+import authMiddleware from "../middleware/Authentication.js";
+import ProfileController from "../controllers/ProfileController.js";
 
 const router = Router();
 
 router.post("/auth/register", AuthController.register);
 router.post("/auth/login", AuthController.login);
+
+// profile routes
+router.get("/profile", authMiddleware, ProfileController.index); //private route
 
 export default router;
