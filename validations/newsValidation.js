6a394fb52c7036129504a14edcec5a1ac33b6db9
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-01 21:03:06 +0600

    NewsValidation Schema

--- validations/newsValidation.js
+++ validations/newsValidation.js
@@ -1,4 +1,7 @@
 import vine from "@vinejs/vine";
+import { CustomErrorReporter } from "./CustomErrorReporter.js";
+
+vine.errorReporter = () => new CustomErrorReporter();
 
 export const newsSchema = vine.object({
   title: vine.string().minLength(5).maxLength(190),
