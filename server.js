9d054e528483623d416961a6b82d5f11e1b03554
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-02 12:06:37 +0600

    secure http header and setup cors

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 5000;
 import fileUpload from "express-fileupload";
+import helmet from "helmet";
+import cors from "cors";
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(fileUpload());
+app.use(helmet());
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.json("Hello World");
